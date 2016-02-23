3fd5d04f9567c1e0ab3279975ef037becde32b04
Bryce Johnson <brycepj@gmail.com>
2016-02-22 22:37:21 -0500

    Add webhook for devbryce jekyll.

--- api.js
+++ api.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var app = express();
-var mailPayload = require('./lib/send_push_notification');
+var fs = require('fs');
 
 app.post('/api/webhook', function (req, res) {
-  mailPayload(req.payload);
-  res.send('Got a POST request');
+  fs.appendFile('buildLog.txt', 'webhook call' + new Date(), function (err) {
+		console.log("There was an error");
+	});
+});
+
+app.post('/api/devbryce-jekyll', function (req, res) {
+	var blogSrcDir = '/home/devbryce/devbryce/devbryce-jekyll';
+	var cmdStr = 'cd ' + blogSrcDir + ' && jekyll build --destination /home/devbryce/public';
+	var child = exec(cmdStr, {async:true});
+	
+	child.stdout.on('data', function(data) {
+		fs.appendFile('buildLog.txt', 'built devbryce' + new Date(), function (err) {
+			console.log("There was an error");
+		});
+	});
+	res.send('A-okay sir');
 });
 
 app.listen(8080, function () {
